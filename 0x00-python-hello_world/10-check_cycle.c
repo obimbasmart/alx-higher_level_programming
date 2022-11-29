fc0f09d2f2145b0a5b8f2b0d23371dbde377625e
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-11-28 17:49:18 -0800

    aded new task

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "list.h"
+
+/**
+ * check_cycle - check if a singly linked list has a cycle
+ * @list: the hed node of the linked list
+ *
+ * Return: 1 if there's a cycle. 0 if not
+ */
+int check_cycle(listint_t *list)
+{
+
+	listint_t *fastptr, *slowptr;
+
+	fastptr = slowptr = list;
+
+	while (list && fastptr && slowptr)
+	{
+		slowptr = slowptr->next;
+		fastptr = fastptr->next->next;
+
+		if (slowptr == fastptr)
+			return (1);
+	}
+
+	return (0);
+}
+
+
+
