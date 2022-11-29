935015fb5cafa1872c2b8b284e44594c0d5e45c0
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-11-29 18:54:40 +0000

    fixed valgrind segmentation fault

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -13,7 +13,7 @@ int check_cycle(listint_t *list)
 
 	fastptr = slowptr = list;
 
-	while (list && fastptr && slowptr)
+	while (list->next && fastptr && slowptr)
 	{
 		slowptr = slowptr->next;
 		fastptr = fastptr->next->next;
