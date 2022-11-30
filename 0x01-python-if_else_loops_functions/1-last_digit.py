d106955d26559fe122e679fce21ec6bf39d9e23d
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-11-30 18:46:31 +0000

    fixed logical errors

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 
 if number < 0:
-    last_digit = (-1 * number) % 10
+    last_digit = ((-1 * number) % 10) * -1
 else:
     last_digit = number % 10
 
@@ -14,7 +14,7 @@ if last_digit > 5:
 
 elif last_digit == 0:
     print(
-          "Last digit of {} is {} and is zero"
+          "Last digit of {} is {} and is 0"
           .format(number, last_digit))
 
 elif last_digit < 6 and last_digit != 0:
