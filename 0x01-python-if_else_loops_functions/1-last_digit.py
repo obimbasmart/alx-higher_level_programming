82b9829d74cb86197dc08c495c0ed05ae7425996
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-11-30 18:37:21 +0000

    added new task

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+if number < 0:
+    last_digit = (-1 * number) % 10
+else:
+    last_digit = number % 10
+
+if last_digit > 5:
+    print(
+          "The last digit of {} is {} and is greater than 5"
+          .format(number, last_digit))
+
+elif last_digit == 0:
+    print(
+          "The last digit of {} is {} and is zero"
+          .format(number, last_digit))
+
+elif last_digit < 6 and last_digit != 0:
+    print(
+          "The last digit of {} is {} and is less than 6 and not 0"
+          .format(number, last_digit))
