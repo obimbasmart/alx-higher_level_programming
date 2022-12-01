2ad590c7c0809cbf81d4fb29a5d5cdf8e7a1a932
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-12-01 16:10:06 +0000

    added new advanced task

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    str_copy = ""
+    i = 0
+    for char in str:
+        if i == n:
+            i += 1
+            continue
+
+        str_copy += char
+        i += 1
+
+    return (str_copy)
