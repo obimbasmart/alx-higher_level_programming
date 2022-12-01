715824ba2331a0b54fa21b08d52d466e2bcacc80
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-12-01 08:04:55 +0000

    added new task

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    '''adds two numbers and return the result
+
+       @a: first number
+       @b: second number
+       Return: sum = a + b
+    '''
+    return (a + b)
