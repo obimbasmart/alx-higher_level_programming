747e5ddbf87e30bd4f75165883e57599e95d32db
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-12-05 02:32:25 +0000

    added new task

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,15 @@
+
+def element_at(my_list, idx):
+    '''function that retrieves an element from a list like in C
+
+       Args:
+            :my_list -  a list
+            :idx - the element index
+
+       Return:
+            :element at position idx or None if none exist
+    '''
+    if (idx < 0 or idx > len(my_list) - 1):
+        return None
+
+    return my_list[idx]
