f83155214d5824112349b7d5448fc6509518885c
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-12-05 10:15:01 +0000

    fixed logical error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -9,7 +9,7 @@ def print_matrix_integer(matrix=[[]]):
     for id_row in range(0, len(matrix)):  # for each row in matrix
         sep = ' '
         for id_col in range(0, len(matrix[id_row])):  # for each int in row
-            if id_col == 2:
+            if id_col == len(matrix[id_row]) - 1:
                 sep = ''
 
             print("{:d}".format(matrix[id_row][id_col]), end=sep)
