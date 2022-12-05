182f30ea98b2afc0e52538a4ab74d7df9e769639
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-12-05 18:07:51 +0000

    fixed logical error

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -10,7 +10,7 @@ def delete_at(my_list=[], idx=0):
         :Return: a new list with element at idx removed
     '''
     if idx < 0 or idx > len(my_list) - 1:
-        return None
+        return my_list
 
     del my_list[idx]
     return my_list
