1c03a50bebf98ed53edd4e4fcbf2f1b5801a0b58
Obimba Smart <obimbasmartchukwunenye@gmail.com>
2022-12-05 08:47:01 +0100

    assume that the list only contains integers

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -8,4 +8,4 @@ def print_reversed_list_integer(my_list=[]):
             :None
     '''
     for idx in range(-1, -1 * (len(my_list) + 1), -1):
-        print("{}".format(my_list[idx]))
+        print("{:d}".format(my_list[idx]))
