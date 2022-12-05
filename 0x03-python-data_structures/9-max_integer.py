da20554d2fd5c35cb93d771c244d1a1f27556631
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-12-05 17:38:40 +0000

    If the list is empty, return None

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -7,6 +7,9 @@ def max_integer(my_list=[]):
             @my_list: list of integers
         :Return: maximum value in @my_list
     '''
+    if len(my_list) == 0:
+        return None
+
     _max = float('-inf')
     for num in my_list:
         if num > _max:
