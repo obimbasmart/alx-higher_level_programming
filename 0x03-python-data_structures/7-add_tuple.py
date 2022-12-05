ac4213633b3c89254b010224ad69b3c98d63c156
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-12-05 16:58:52 +0000

    added new task

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    ''' function that adds 2 tuples.
+
+        :Return: tuple with two elements
+    '''
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
+
+    a = b = 0
+
+    if len_a == 0:
+        pass
+    elif len_a == 1:
+        a = tuple_a[0]
+    elif len_a > 1:
+        a = tuple_a[0]
+        b = tuple_a[1]
+
+    if len_b == 0:
+        pass
+    elif len_b == 1:
+        a += tuple_b[0]
+    elif len_a > 1:
+        a += tuple_b[0]
+        b += tuple_b[1]
+
+    return (a, b)
