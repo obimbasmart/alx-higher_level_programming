1e6762d9cea253d728ebf1941cd4fb39b1e1a440
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-12-07 07:53:09 +0000

    fixed undefined reference error

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -42,3 +42,31 @@ int is_palindrome(listint_t **head)
 
 	return (1);
 }
+
+
+/**
+ * listint_len - return the number of elements in a linked listint_t
+ * @h: the head of the list
+ *
+ * Return: the number of nodes in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	unsigned int n_nodes;
+
+	n_nodes = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	else
+	{
+		do {
+			n_nodes++;
+			h = h->next;
+		} while (h != NULL);
+	}
+	return (n_nodes);
+}
