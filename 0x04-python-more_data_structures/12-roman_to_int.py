9a417da2f8191973d8f2a075e4ee86266a08a44c
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-12-08 06:58:52 +0000

    added new task

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    ''' converts a Roman numeral to an integer
+
+        Args:
+            @roman_string: roman representation
+        Return:
+            integer representation
+    '''
+    if roman_string is None:
+        return 0
+
+    int_sum = 0
+    roman_numerals = {
+                'I': 1, 'V': 5, 'X': 10,
+                'L': 50, 'C': 100,
+                'D': 500, 'M': 1000}
+
+    for idx in range(-1, -1 * (len(roman_string) + 1), -1):
+        roman_char = roman_string[idx]
+
+        if idx != -1:
+            _roman_char = roman_string[idx + 1]
+            if roman_numerals[roman_char] < roman_numerals[_roman_char]:
+                int_sum -= roman_numerals[roman_char]
+            else:
+                int_sum += roman_numerals[roman_char]
+        else:
+            int_sum += roman_numerals[roman_char]
+
+    return int_sum
