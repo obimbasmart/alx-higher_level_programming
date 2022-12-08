5badc2566e6ff85dddb6532e4721051bcaffabfe
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-12-08 18:38:57 +0000

    not allowed to use for or while

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return (list(map(lambda _list: [i**2 for i in _list], matrix)))
+    return (list(map(lambda _list: list(map(lambda x: x**2, _list)), matrix)))
