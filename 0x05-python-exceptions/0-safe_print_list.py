c094bf81664e239bd0f7bda48bffeba0388a27e4
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-12-20 09:11:03 +0000

    handle edge case

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,7 +10,7 @@ def safe_print_list(my_list=[], x=0):
             the real number of elements printed
     '''
     if my_list is None or x <= 0:
-        return 0
+        return None
 
     for idx in range(x):
         try:
