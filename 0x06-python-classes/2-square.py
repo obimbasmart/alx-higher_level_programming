1f4be34f23598ebbf9b6b730ff722e95a2ac25a9
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-12-20 22:43:27 +0000

    added new task

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+""" This module holds a blueprint for a square shape"""
+
+
+class Square:
+    """ a class Square that defines a square by: (based on 0-square.py)
+    """
+
+    def __init__(self, size=0):
+        """ initialization of object data
+
+        Args:
+            size (int): size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
