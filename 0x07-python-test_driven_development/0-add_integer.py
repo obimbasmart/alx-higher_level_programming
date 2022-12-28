b2e3ab98f05723400394c4e4d7e24e6c8b95cea6
Smartify-Tech <obimbasmartchukwunenye@gmail.com>
2022-12-28 22:12:10 +0100

    initial commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+""" This module contains a function that adds two integers"""
+
+
+def add_integer(a, b=98):
+    """adds two integers a and b
+
+    Args
+        a(int): first int
+        b(int): second int
+    Return:
+        sum(int): sum of a and b
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+        return
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+        return
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return a + b
