ecf28b478f3a423b5d7d2035ad272cbdec2d3447
Smartify-Tech <obimbasmartchukwunenye@gmail.com>
2022-12-30 23:39:12 +0100

    added a new task

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+"""
+function that prints a square with the character #.
+
+Prototype: def print_square(size):
+size is the size length of the square
+size must be an integer, otherwise raise a
+    TypeError exception with the message size must be an integer
+if size is less than 0, raise a ValueError exception with the message
+    size must be >= 0
+if size is a float and is less than 0, raise a TypeError exception
+    with the message size must be an integer
+"""
+
+
+def print_square(size):
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    [print("#" * size) for i in range(size)]
