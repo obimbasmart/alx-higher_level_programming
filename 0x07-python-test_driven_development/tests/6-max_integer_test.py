69e50d998ca9924d169b805656264a2cbd2b8aaf
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-12-31 22:20:40 +0000

    added more test cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,10 +11,12 @@ class TestMaxInteger(unittest.TestCase):
     """my unittest class"""
 
     def test_valid_args(self):
-        self.assertEqual(max_integer([]), None)
-        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
-        self.assertEqual(max_integer([0]), 0)
-        self.assertEqual(max_integer([-5, -3, 400, 99]), 400)
+        self.assertEqual(max_integer([]), None)   # empty list
+        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)  # valid list
+        self.assertEqual(max_integer([0]), 0)  # single element
+        self.assertEqual(max_integer([-5, -3, 400, 99]), 400)  # -ve & +ve nums
 
     def test_exceptions(self):
-        self.assertRaises(TypeError, max_integer(4))
+        self.assertRaises(TypeError, max_integer(4))   # invalid arg
+        self.assertRaises(TypeError, max_integer("list"))   # invalid arg
+        self.assertRaises(ValueError, max_integer([1, 4, 5, "six"]))
