7565f2b34589568795fce15df55c9e3362dc3b44
Smartify-Tech <obimbasmartchukwunenye@gmail.com>
2023-01-03 15:35:14 +0100

    initial commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+"""
+An empty class `Rectangle` that defines a Rectangle object
+"""
+
+
+class Rectangle:
+    """an empty rectangle object"""
+
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,5 @@
+# Python - More Classes and Objects
+
+## Mandatory
+
+0. An empty class `Rectangle` that defines a rectangle:
