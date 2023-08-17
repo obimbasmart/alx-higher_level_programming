78fde56953865dd34887c1f3def52378864741a5
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-08-17 19:37:12 +0100

    added an advanced task

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -12,5 +12,5 @@ def weight_average(my_list=[]):
     if not my_list:
         return 0
 
-    return sum([tup[0] * tup[1] for tup in my_list]) /
-    sum([tup[1] for tup in my_list])
+    return sum([tup[0] * tup[1] for tup in my_list]) / sum(
+        [tup[1] for tup in my_list])
