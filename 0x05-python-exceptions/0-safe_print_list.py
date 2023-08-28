48e49a3ebdd47f3ced9661b9eccc57d4f0d1e75c
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-08-28 21:59:45 +0100

    new: add first task

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    '''
+        prints x elements of a list
+        Args:
+            @my_list: list
+        Return:
+            number of elements printed
+    '''
+    if x == 0:
+        return 0
+
+    for idx in range(x):
+        try:
+            var = my_list[idx]
+        except IndexError:
+            print()
+            return idx
+        else:
+            print(var, end='')
+
+    print()
+    return (idx + 1)
