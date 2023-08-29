24f1733823a7bd34425f7fa44f203e46bc7c6d47
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-08-29 06:33:16 +0100

    fix: incorrect output - logical err

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,8 @@ def safe_print_list(my_list=[], x=0):
         Return:
             number of elements printed
     '''
-    if (x == 0):
+    if (x == 0 or not my_list):
+        print()
         return (0)
 
     for idx in range(x):
