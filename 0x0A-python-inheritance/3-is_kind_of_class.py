7bcc7d79dcd0108748a7e340b46fff6a1e04ff67
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-09-11 23:39:23 +0100

    new: add new task

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+This module a function that returns True if the object
+is an instance of, or if the object is an instance of a
+class that inherited from, the specified class ; otherwise False.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """check if the @obj is an instance of,
+    or if the object is an instance of a
+    class that inherited from @a_class"""
+    return isinstance(obj, a_class)
