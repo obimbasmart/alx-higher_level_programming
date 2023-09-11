16a85aaaa218f7003ff4d17887a0a48231bad15a
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-09-11 23:49:11 +0100

    new: add new task

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+A class BaseGeometry (based on 5-base_geometry.py).
+"""
+
+
+class BaseGeometry:
+    """empty geometry class"""
+
+    def area(self):
+        """return area of geometry"""
+        raise Exception("area() is not implemented")
