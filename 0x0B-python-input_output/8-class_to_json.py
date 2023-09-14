4392c83d2e4bb2a0587c19a9de61210df7660fda
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-09-14 18:51:28 +0100

    new: add new task

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+a function that returns the dictionary description with
+simple data structure (list, dictionary, string, integer and boolean)
+for JSON serialization of an object:
+"""
+
+
+def class_to_json(obj):
+    """return dictionary discprtion for JSON
+    of an object
+    """
+    return obj.__dict__
