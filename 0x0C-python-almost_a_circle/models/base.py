41a272303e2ecc5560c3ded96df315876dfa3333
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-09-19 00:12:38 +0100

    new: add json serializer

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,3 +19,15 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries
+        Args:
+            list_dictionaries(list): list of objects of class Base
+        Return:
+            (list): a list of json(str) representation of objects
+        """
+        if list_dictionaries is None or not len(list_dictionaries):
+            return "[]"
+        return json.dumps(list_dictionaries)
