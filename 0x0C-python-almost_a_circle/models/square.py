dece1ae50042d7a90ccd9e6e270a5382519cfce4
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-09-18 22:59:26 +0100

    new: add new Square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""class Square that inherits from Rectangle
+
+a Square is a special Rectangle, so it makes sense this class
+Square inherits from Rectangle.
+"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class Square inherits from Rectangle
+    A square is a special Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """object instantiation
+        Args:
+            size(int): size of square
+            x(int): x coordinate of origin
+            y(int): y coordinate of origin
+            id(int): unique object identification
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """return string representation of Rectangle instance"""
+        return "[{}] ({}) {}/{} - {}".format(
+            self.__class__.__name__, self.id,
+            self.x, self.y,
+            self.width
+        )
