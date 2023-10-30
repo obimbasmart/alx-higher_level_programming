b7e985bc95f78d57c9f19e9a0056633d77072fcb
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-10-30 01:02:08 +0100

    fix: logical error

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,5 @@
 -- convert database and table to UTF-8
 
+USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
