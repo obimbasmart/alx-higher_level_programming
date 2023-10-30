c0e3a9a07e13635ce001b6a200cd3ae2138238ca
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-10-30 17:04:22 +0100

    fix: syntax error

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,5 +6,5 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
 	id INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
 	state_id INT NOT NULL,
-	FOREIGN KEY(state_id) REFERENCES state(id)
+	FOREIGN KEY(state_id) REFERENCES states(id)
 );
