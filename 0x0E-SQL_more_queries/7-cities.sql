b58a5c75b7ef816e4da10c6928fd81ddca8b6e2d
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-10-30 18:18:21 +0100

    fix: logical error

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,5 +6,6 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
 	id INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
 	state_id INT NOT NULL,
-	FOREIGN KEY(state_id) REFERENCES states(id)
+	FOREIGN KEY(state_id) REFERENCES states(id),
+	name VARCHAR(256) NOT NULL
 );
