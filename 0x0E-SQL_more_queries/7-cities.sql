244363326663c904859ee1a5645d5eac1011617c
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-10-30 16:51:28 +0100

    new: add new task

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- create database, and table
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
+	id INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
+	state_id INT NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES state(id)
+);
