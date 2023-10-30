ab1dfbb29725be0747ea393ce83ff0ff68b71f2d
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-10-30 21:06:44 +0100

    fix: use only one SELCT stmt

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,7 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 
-SELECT name, COUNT(show_id) AS number_of_shows
-FROM 
-	(SELECT tv_genres.name, tv_show_genres.show_id 
-	 FROM tv_genres
-	 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id) AS deriv 
-GROUP BY name ORDER BY number_of_shows DESC;
+SELECT tv_genres.name, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY name
+ORDER BY number_of_shows DESC;
