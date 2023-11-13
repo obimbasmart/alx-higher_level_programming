9627f177aedbd27e9163279d8bdf343b07742356
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-11-13 17:03:54 +0100

    fix: logical error

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,11 +3,5 @@
 if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
-  process.argv.forEach((item, index) => {
-    if (index < 2) {
-      return;
-    }
-
-    console.log(item);
-  });
+  console.log(process.argv[2]);
 }
