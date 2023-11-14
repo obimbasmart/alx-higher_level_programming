df077cd3c33716da432871d46e214072e9a9509b
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-11-14 16:19:36 +0100

    fix: requirement

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,11 @@
 // find second largest number
 
 let arr = process.argv; arr.shift(); arr.shift(); // remove first two items
-arr = arr.map((item) => Number.parseInt(item, 10));
-arr.sort((a, b) => b - a); // sort in ascending order
-console.log(arr[1]);
+
+if (arr.length === 0 || arr.length === 1) {
+  console.log(0);
+} else {
+  arr = arr.map((item) => Number.parseInt(item, 10));
+  arr.sort((a, b) => b - a); // sort in ascending order
+  console.log(arr[1]);
+}
