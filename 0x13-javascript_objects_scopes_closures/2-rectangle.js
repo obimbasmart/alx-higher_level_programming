5f981bdfaca41919cbee9022ec670cbad1c8352a
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-11-17 01:38:23 +0100

    new: add new task

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// create a Rectangle class
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return;
+    }
+
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
