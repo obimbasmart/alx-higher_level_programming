824d3a182bc832953fca3976c5f85d149eccb71f
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-11-18 12:47:27 +0100

    new: add new task

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+'''mysql->python: lists all states from the database
+hbtn_0e_0_usa with name starting with N'''
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    _, user_name, passwd, db_name = sys.argv
+    db = MySQLdb.connect(host='localhost', user=user_name,
+                         passwd=passwd, db=db_name)
+    cur = db.cursor()
+    cur.execute('''SELECT * FROM states
+                   WHERE name LIKE "N%"
+                ''')
+    query_result = cur.fetchall()
+
+    for item in query_result:
+        print(item)
+
+    cur.close()
+    db.close()
