6341608a55bbd980864480ebb722cdd102b6545a
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-11-18 13:33:38 +0100

    new: add new task

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+'''mysql->python: filter states by name'''
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    _, user_name, passwd, db_name, state_name = sys.argv
+    db = MySQLdb.connect(host='localhost', user=user_name,
+                         passwd=passwd, db=db_name)
+    cur = db.cursor()
+    cur.execute(''' SELECT * FROM states
+                    WHERE name = "{}"
+                '''.format(state_name))
+    query_result = cur.fetchall()
+
+    for item in query_result:
+        print(item)
+
+    cur.close()
+    db.close()
