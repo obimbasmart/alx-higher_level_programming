8973ca236f366eafb88b4f69a3de4253cbc0fc4f
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-11-18 12:13:27 +0100

    new: add first task

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    _, user_name, passwd, db_name = sys.argv
+    db = MySQLdb.connect(host='localhost', user=user_name,
+                         passwd=passwd, db=db_name)
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states')
+    query_result = cur.fetchall()
+
+    for item in query_result:
+        print(item)
+
+    cur.close()
+    db.close()
