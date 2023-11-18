cf46bbe6df724a2b38b8eae05a04d223829a6968
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-11-18 16:38:47 +0100

    new: add new task

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+'''mysql->python: filter states by name'''
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    _, user_name, passwd, db_name, state_name = sys.argv
+    db = MySQLdb.connect(host='localhost', user=user_name,
+                         passwd=passwd, db=db_name)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.name
+                FROM cities
+                INNER JOIN states ON cities.state_id = states.id
+                WHERE BINARY states.name = %s""", (state_name,))
+
+    query_result = cur.fetchall()
+    output = ', '.join([item[0] for item in query_result])
+    print(output)
+    cur.close()
+    db.close()
