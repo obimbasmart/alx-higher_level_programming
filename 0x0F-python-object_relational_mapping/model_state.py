9ddc352b1b72ef29da6fbca4f577651205ec8d61
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-11-21 02:18:32 +0100

    fix: syntax error

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
 
-"""file that contains the class definition of a State"""
+"""file that contains the class definition of a State,
+an instance of Base = declarative_base()"""
 
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, Integer, String
-import sys
-
-
-_, user_name, passwd, db_name = sys.argv
-engine = create_engine(
-    'mysql+mysqldb://{}:{}@localhost/{}'.format(user_name, passwd, db_name))
-session = sessionmaker(bind=engine)
 
 Base = declarative_base()
 
 
 class State(Base):
-    """ class definition of a State, an instance of declarative_base"""
+    """ class definition of a State"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
