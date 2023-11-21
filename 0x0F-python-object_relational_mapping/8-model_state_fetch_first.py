0afa399116ffe3882d25a69b15383ee5fd27a8d0
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-11-21 03:24:02 +0100

    new: add new task

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""script that lists all State objects from the database"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    _, user_name, passwd, db_name = sys.argv
+    engine = create_engine(
+        'mysql+mysqldb://localhost/{}'.format(db_name),
+        connect_args={'user': user_name, 'password': passwd})
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    first_state = session.query(State).first()
+
+    print('{}: {}'.format(first_state.id, first_state.name))
+    session.close()
