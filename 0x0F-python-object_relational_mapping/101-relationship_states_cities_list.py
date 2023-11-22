1fb205bb3b21d5cafa2236b8e9d87dfcbbc2848a
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-11-22 20:59:19 +0100

    fix: document module

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""lists all State objects, and corresponding
+City objects, contained in the database"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
