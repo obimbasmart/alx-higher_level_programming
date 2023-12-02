f5194ab302e00c5eb46d57c971b01d540ec34ef3
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-12-02 15:21:23 +0100

    fix: make file 3 lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # display the size of the body of a HTTP response
-
-url="$1"
-curl -sI "$url" | grep "Content-Length" | grep -oP '\d+' 
+curl -sI "$1" | grep "Content-Length" | grep -oP '\d+' 
