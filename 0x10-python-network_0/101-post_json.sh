25a73ea42520cb03023a5bd4d7c382cd3a0cf05e
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-12-03 06:29:01 +0100

    fix: error

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a DELETE request to an URL
-curl -s -X POST -H "Content-Type: application/json" --data @my_json "$1"
+curl -s -X POST -H "Content-Type: application/json" --data "@$2" "$1"
