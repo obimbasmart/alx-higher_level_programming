5e5087bee392f6f8100198faff16f9deeed96fd7
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-12-04 09:45:54 +0100

    new: send email data to POST request

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" cript that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    _, url, email = sys.argv
+    params = {
+        'email': email
+    }
+    res = requests.request('POST', url, data=params)
+    print(res.text)
