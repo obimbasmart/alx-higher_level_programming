bc5620f1e2f9a34ac1ccc8314df44c73e198873f
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-12-04 07:43:38 +0100

    fix: typo error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ url = 'https://alx-intranet.hbtn.io/status'
 with urllib.request.urlopen(url) as response:
     html = response.read()
 
-print("Bad response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
       .format(type(html), html, str(html, 'UTF-8')
               ))
