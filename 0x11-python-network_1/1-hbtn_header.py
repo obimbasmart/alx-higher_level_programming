f6bc9e6821a6592eff329d2c0028b73043eea400
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-12-04 08:07:13 +0100

    fix: import should not execute code"

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""sends a request to the URL and displays the
+""" sends a request to the URL and displays the
 value of the X-Request-Id variable found in the
 header of the response."""
-
 import urllib.request
 import sys
 
-_, url = sys.argv
-with urllib.request.urlopen(url) as response:
-    r_header = response.info()
-
-print(r_header.get('X-Request-Id'))
+if __name__ == "__main__":
+    _, url = sys.argv
+    with urllib.request.urlopen(url) as response:
+        r_header = response.info()
+    print(r_header.get('X-Request-Id'))
