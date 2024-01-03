76347f408af0aba79e9fce60057aadb5eb709409
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-01-03 22:34:46 +0100

    fix: error

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,5 +5,5 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (err, response, body) {   // eslint-disable-line
-  console.log(response.statusCode);
+  console.log(`code: ${response.statusCode}`);
 });
