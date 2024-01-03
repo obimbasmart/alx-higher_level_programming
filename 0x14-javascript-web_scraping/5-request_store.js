4e7ffd08fc89ddf3b7636f094c1db00a07bb711a
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-01-03 23:28:18 +0100

    add: new task

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// write content from webpage and store in file
+
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const filename = process.argv[3];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  fs.writeFile(filename, body, 'utf-8', function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
