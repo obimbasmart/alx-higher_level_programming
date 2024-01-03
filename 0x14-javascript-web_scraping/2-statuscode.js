a76c4dd97c1ee5ca1479493d85b0f06bcbcd4d0e
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-01-03 22:32:05 +0100

    add: new task

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// write content from file
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response, body) {   // eslint-disable-line
+  console.log(response.statusCode);
+});
