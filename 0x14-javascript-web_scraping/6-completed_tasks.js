e9c6ddf7db719efa7bbbf6d6a41604ebffa1e066
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-01-04 10:07:36 +0100

    add: new task

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+// computes the number of tasks completed by user id.
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const allTodos = JSON.parse(body);
+  const allUsers = {};
+
+  allTodos.forEach(task => {
+    if (!Object.prototype.hasOwnProperty.call(allUsers, task.userId)) {
+      allUsers[task.userId] = 0;
+    }
+
+    if (task.completed) {
+      allUsers[task.userId]++;
+    }
+  });
+
+  console.log(allUsers);
+});
