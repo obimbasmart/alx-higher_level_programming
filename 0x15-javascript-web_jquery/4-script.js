b9fd31807f18f13b6a2f0f7ef462733b55f5d2cf
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-01-12 09:53:57 +0100

    add new task

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,10 @@
+
+/*
+toggles the class of the <header> element when the user clicks on the tag DIV#toggle_header:
+*/
+
+$(document).ready(function () {
+  $('#toggle_header').on('click', function () {
+    $('header').toggleClass('red green');
+  });
+});
