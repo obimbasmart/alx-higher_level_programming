844377d4b4bb23683d28cd66a89f9c0d897bb249
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-01-12 10:09:01 +0100

    add new item

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,9 @@
+/*
+adds a <li> element to a list when the user clicks on the tag DIV#add_item:
+*/
+
+$(document).ready(function () {
+  $('#add_item').on('click', function () {
+    $('ul.my_list').append('<li>Item</li>');
+  });
+});
