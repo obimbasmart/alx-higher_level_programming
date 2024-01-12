a539da9efce2f328aeaf637ad6fdc87201be223b
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-01-12 15:07:53 +0100

    add new advanced task

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,10 +1,13 @@
 /* script that fetches from
  *  https://hellosalut.stefanbohacek.dev/?lang=fr and displays the value
  * of hello from that fetch in the HTML tag DIV#hello.
+ * 
+ * he translation must be fetched when the user clicks on 
+ * INPUT#btn_translate OR presses ENTER when the focus is on INPUT#language_code
 */
 
 $(document).ready(function () {
-  $('#btn_translate').click(function () {
+  const getData = function () {
     $.ajax(
       {
         url: 'https://hellosalut.stefanbohacek.dev/',
@@ -23,5 +26,14 @@ $(document).ready(function () {
         }
       }
     );
+  };
+
+  $('#language_code').on('keypress', function (key) {
+    if (key.which === 13) {
+      getData();
+    }
+  });
+  $('#btn_translate').on('click', function () {
+    getData();
   });
 });
